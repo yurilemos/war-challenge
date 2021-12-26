2adb86b55dc556bc4925b137c42ec6bd66cae7b6
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2021-12-26 17:58:42 -0300

    finished project

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Yuri Lemos on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
